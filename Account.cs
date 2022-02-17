65e5ecc7fff7a1fc3f9f278f5dc8b3d9bf687cc8
tristonh35 <tristonhall35@yahoo.com>
2022-02-16 18:54:03 -0800

    Add project files.

---
+++ Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a customers bank account
+    /// </summary>
+    internal class Account
+    {
+        /// <summary>
+        /// Creates an account with a specific owner and a balance of 0
+        /// </summary>
+        /// <param name="accOwner">The customer full name that owns the account</param>
+        public Account(string accOwner)
+        {
+            Owner = accOwner;
+        }
+        /// <summary>
+        /// The account owners full name (first and last) 
+        /// </summary>
+        public string Owner { get; set; }
+        /// <summary>
+        /// The amount of money currently in the account
+        /// </summary>
+        public double Balance { get; private set; }
+        /// <summary>
+        /// Add a specified amount of money to the account
+        /// </summary>
+        /// <param name="amt">The positive amount to deposit</param>
+        
+        public void Deposit(double amt)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Withdraw a specified amount of money from the account
+        /// </summary>
+        /// <param name="amt">The positive amount to withdraw</param>
+        public void Withdraw(double amt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
